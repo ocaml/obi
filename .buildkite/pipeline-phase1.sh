f82090b95b3f434b5d9dee78e42c841fa3a3a95c
Anil Madhavapeddy <anil@recoil.org>
2018-04-10 19:12:48 +0100

    fix download syntax

--- .buildkite/pipeline-phase1.sh
+++ .buildkite/pipeline-phase1.sh
@@ -8,7 +8,7 @@ for i in Dockerfile.*; do
   f=$(echo $i | sed -e 's/^Dockerfile.//g') 
   cat <<EOM
   - label: $f-$1
-    command: "buildkite-agent artifact download phase1-$1/Dockerfile.$f && cat phase1-$1/Dockerfile.$f && docker build -f phase1-$1/Dockerfile.$f -t $f phase1-$1"
+    command: "buildkite-agent artifact download phase1-$1/Dockerfile.$f . && cat phase1-$1/Dockerfile.$f && docker build -f phase1-$1/Dockerfile.$f -t $f phase1-$1"
 
 EOM
 done
