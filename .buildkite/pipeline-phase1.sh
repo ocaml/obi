288e00755caf09071d41e5100e5776607530269f
Anil Madhavapeddy <anil@recoil.org>
2018-04-10 19:22:09 +0100

    build and push!

--- .buildkite/pipeline-phase1.sh
+++ .buildkite/pipeline-phase1.sh
@@ -8,8 +8,12 @@ for i in Dockerfile.*; do
   f=$(echo $i | sed -e 's/^Dockerfile.//g') 
   cat <<EOM
   - label: $f-$1
-    command: "buildkite-agent artifact download phase1-$1/Dockerfile.$f . && cat phase1-$1/Dockerfile.$f && docker build -f phase1-$1/Dockerfile.$f -t $f phase1-$1"
-
+    command:
+      - buildkite-agent artifact download phase1-$1/Dockerfile.$f .
+      - cat phase1-$1/Dockerfile.$f
+      - docker build --no-cache --pull -f phase1-$1/Dockerfile.$f -t ocaml/opam2-staging:$f-opam-linux-$1 phase1-$1
+    plugins:
+      docker-login#v1.0.0:
+        username: avsm
 EOM
 done
-#  echo "  - label: $f\n    command: \"buildkite-agent artifact download phase1-$1/Dockerfile.$f && cat phase1-$1/Dockerfile.$f && docker build -f phase1-$1/Dockerfile.$f -t $f\"\n"
