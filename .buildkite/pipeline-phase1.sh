dc7b4806c4ca41d376e12cc0c69c7c82aa577b1e
Anil Madhavapeddy <anil@recoil.org>
2018-04-10 19:04:38 +0100

    fix pipeline syntax

--- .buildkite/pipeline-phase1.sh
+++ .buildkite/pipeline-phase1.sh
@@ -6,5 +6,10 @@ echo "steps:"
 cd $2/phase1-$1
 for i in Dockerfile.*; do
   f=$(echo $i | sed -e 's/^Dockerfile.//g') 
-  echo "  - label: '$f'\n    command: \"buildkite-agent artifact download phase1-$1/Dockerfile.$f && cat phase1-$1/Dockerfile.$f && docker build -f phase1-$1/Dockerfile.$f -t $f\"\n"
+  cat <<EOM
+  - label: $f-$1
+    command: "buildkite-agent artifact download phase1-$1/Dockerfile.$f && cat phase1-$1/Dockerfile.$f && docker build -f phase1-$1/Dockerfile.$f -t $f phase1-$1"
+
+EOM
 done
+#  echo "  - label: $f\n    command: \"buildkite-agent artifact download phase1-$1/Dockerfile.$f && cat phase1-$1/Dockerfile.$f && docker build -f phase1-$1/Dockerfile.$f -t $f\"\n"
