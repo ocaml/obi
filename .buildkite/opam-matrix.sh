1b8263db048a12b5dd667d6a4ecfe01f65bd78fd
Anil Madhavapeddy <anil@recoil.org>
2018-04-30 14:23:15 +0100

    gather logs, dont make them async

--- .buildkite/opam-matrix.sh
+++ .buildkite/opam-matrix.sh
@@ -10,7 +10,6 @@ build() {
   cat <<EOL
 - trigger: "opam-bulk"
   label: ":docker: $distro :camel: $ov :github: $srev"
-  async: true
   build:
     env:
       DISTRO: "$distro"
@@ -23,3 +22,16 @@ build "debian-9" "4.07"
 build "debian-9" "4.06"
 build "debian-9" "4.06+default-unsafe-string"
 build "debian-9" "4.06+flambda"
+build "alpine-3.7" "4.06"
+
+cat <<EOL
+- label: "Rebuild index"
+  command:
+    - rm -rf obi-logs
+    - git clone --depth=1 git://github.com/avsm/obi-logs
+    - find obi-logs/batch -type f
+  agents:
+    arch: "amd64"
+    docker: "true"
+    linux: "true"
+EOL
