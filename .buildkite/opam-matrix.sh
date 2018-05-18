8fc5c11210b417c3979773385c87de59615c0ebf
Anil Madhavapeddy <anil@recoil.org>
2018-05-18 11:58:26 +0100

    include arch in label

--- .buildkite/opam-matrix.sh
+++ .buildkite/opam-matrix.sh
@@ -10,7 +10,7 @@ build() {
   arch=$3
   cat <<EOL
 - trigger: "opam-bulk"
-  label: ":docker: $distro :camel: $ov :github: $srev"
+  label: ":docker: $distro :camel: $ov :github: $srev :compute: $arch"
   build:
     env:
       DISTRO: "$distro"
@@ -28,19 +28,14 @@ index() {
 EOL
 }
 
-build "debian-9" "4.06" "arm64"
-index
-exit 0
 build "debian-9" "4.07" "amd64"
 build "debian-9" "4.06" "amd64"
 build "debian-9" "4.05" "amd64"
 build "debian-9" "4.04" "amd64"
 build "debian-9" "4.03" "amd64"
-index
 build "debian-9" "4.08" "amd64"
 build "debian-9" "4.06+default-unsafe-string" "amd64"
 build "debian-9" "4.06+flambda" "amd64"
-index
 build "alpine-3.7" "4.06" "amd64"
 build "ubuntu-18.04" "4.06" "amd64"
 build "fedora-27" "4.06" "amd64"
