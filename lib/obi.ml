eb228da8f2870cefad17b79ff89d4c5838fcc055
Anil Madhavapeddy <anil@recoil.org>
2018-05-23 17:49:10 +0100

    rearrange

--- lib/obi.ml
+++ lib/obi.ml
@@ -5,30 +5,37 @@ module Ocaml_version = struct
   include Ocaml_version_sexp
 end
 
-type build_result = {
-  code: [`Signaled of int | `Exited of int];
-  start_time: float;
-  end_time: float;
-} [@@deriving sexp]
+module Builds = struct
+  type build_result = {
+    code: [`Signaled of int | `Exited of int];
+    start_time: float;
+    end_time: float;
+  } [@@deriving sexp]
 
-type pkg =
-  { name: string
-  ; versions: (string * build_result) list }
-  [@@deriving sexp]
+  type pkg =
+    { name: string
+    ; versions: (string * build_result) list }
+    [@@deriving sexp]
 
-type params = {
-  arch: Dockerfile_distro.arch;
-  distro : Dockerfile_distro.t;
-  ov: Ocaml_version.t;
-} [@@deriving sexp]
+  type params = {
+    arch: Dockerfile_distro.arch;
+    distro : Dockerfile_distro.t;
+    ov: Ocaml_version.t;
+  } [@@deriving sexp]
 
 type batch =
   {rev: string; params: params; pkgs: pkg list}
   [@@deriving sexp]
+end
 
 module Index = struct
   type maintainers = (string * string) list [@@deriving sexp]
   type tags = (string * string list) list [@@deriving sexp]
+  type params = {
+    arch: Dockerfile_distro.arch;
+    distro : Dockerfile_distro.t;
+    ov: Ocaml_version.t;
+  } [@@deriving sexp]
   type metadata =
     { rev: string;
       params: params;
