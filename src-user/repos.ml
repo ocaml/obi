31a9a9016f1c7ee8e44ffbd3ebd90317f176deef
Anil Madhavapeddy <anil@recoil.org>
2018-05-31 14:49:04 +0100

    optional network

--- src-user/repos.ml
+++ src-user/repos.ml
@@ -35,19 +35,29 @@ let run_git_in_repo ~repo args =
 let run_git args =
   OS.Cmd.(run (Cmd.(v "git" %% of_list args)))
 
-let init () =
+let init ?(network=true) () =
   let d = obi_dir () in
   Logs.info (fun l -> l "Initialising in %a" Fpath.pp d);
   OS.Dir.create ~path:true d >>= fun _ ->
   let local_logs_repo = Fpath.(d / "obi-logs") in
   OS.Dir.exists local_logs_repo >>= fun repo_exists ->
   (if repo_exists then begin
-    Logs.debug (fun l -> l "Fetching latest Obi logs");
-    run_git_in_repo ~repo:local_logs_repo ["fetch"; "-q"; "origin"; "index"] >>= fun () ->
-    run_git_in_repo ~repo:local_logs_repo ["reset"; "-q"; "--hard"; "@{u}"]
+    if network then begin
+      Logs.debug (fun l -> l "Fetching latest Obi logs");
+      run_git_in_repo ~repo:local_logs_repo ["fetch"; "-q"; "origin"; "index"] >>= fun () ->
+      run_git_in_repo ~repo:local_logs_repo ["reset"; "-q"; "--hard"; "@{u}"]
+      end else begin
+      Logs.debug (fun l -> l "Using existing Obi logs");
+      Ok ()
+    end;
   end else begin
-    Logs.debug (fun l -> l "Cloning fresh Obi logs");
-    run_git ["clone"; "-q"; "--depth=1"; "-b"; "index"; remote_logs_repo (); Cmd.p local_logs_repo]
+    if network then begin
+      Logs.debug (fun l -> l "Cloning fresh Obi logs");
+      run_git ["clone"; "-q"; "--depth=1"; "-b"; "index"; remote_logs_repo (); Cmd.p local_logs_repo]
+    end else begin
+      Logs.err (fun l -> l "Must use --network=true to fetch initial logs");
+      Error (`Msg "Must use --network=true to fetch initial logs")
+    end
   end) >>= fun () ->
   (* TODO check version in obi-logs *)
   (* TODO store multiple versions based on date? *)
