b057c15e84f98025482db6278caa490b6441ac92
Anil Madhavapeddy <anil@recoil.org>
2018-06-01 18:51:52 +0100

    test

--- .buildkite/obi-binary.sh
+++ .buildkite/obi-binary.sh
@@ -12,7 +12,7 @@ COPY . /home/opam/src/
 RUN sudo chown -R opam /home/opam/src
 RUN opam pin add -n --dev dockerfile-opam
 RUN opam pin add -n --dev ocaml-version
-RUN opam install --deps-only /home/opam/src
+RUN opam install -y -j10 --deps-only /home/opam/src
 RUN cd /home/opam/src && opam exec -- jbuilder build
 FROM ocaml/opam2-staging
 COPY --from=0 /home/opam/src/_build/install/default/bin/obi-buildkite /usr/bin/obi-buildkite
