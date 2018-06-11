5757101ae71a6415a5529af1e469eb6217a220c1
Anil Madhavapeddy <anil@recoil.org>
2018-06-11 19:03:55 +0100

    more doc

--- src/lib/obi.mli
+++ src/lib/obi.mli
@@ -20,7 +20,9 @@
 module Index : sig
 
   type deps = (string * string * [ `Fail | `Ok | `Skipped ]) list [@@deriving sexp]
-  (** [deps] is a tuple of () *)
+  (** [deps] is a list of package dependencies, where each entry is a
+      tuple of [name, version, status]  *)
+
   type result =  [
     | `Ok
     | `Fail of int * deps
@@ -29,28 +31,45 @@ module Index : sig
     | `No_sources of string list
     | `Solver_failure
   ] [@@deriving sexp]
+  (** [result] represents the exit result of an [opam] invocation. The raw
+      exit codes are parsed to look for special opam exit codes, which indicate
+      errors such as the internal solver failing to find a result. *)
+
   val pp_result : Format.formatter -> result -> unit
+  (** [pp_result ppf result] will print a human-readable result to formatter [ppf] *)
+
   type params = {
-    arch: Dockerfile_distro.arch;
-    distro : Dockerfile_distro.t;
-    ov: Ocaml_version.t;
+    arch: Dockerfile_distro.arch; (** CPU architecture *)
+    distro : Dockerfile_distro.t; (** Operating system distribution *)
+    ov: Ocaml_version.t; (** OCaml compiler version *)
   } [@@deriving sexp]
+  (** [params] represents some of the build parameters that opam packages are tested
+    against. These include the CPU architecture, OS distribution and OCaml compiler
+    version. *)
+
   type metadata =
-    { rev: string;
-      params: params;
-      build_result: result;
-      start_time: float;
-      end_time: float;
-      log: string list;
+    { rev: string;  (** opam-repository git revision hash that this run was built against *)
+      params: params; (** build {!params} for this run *)
+      build_result: result; (** result of the execution of this set of parameters *)
+      start_time: float; (** wall clock start time of the build in Unix seconds *)
+      end_time: float; (** wall clock end time of the build in Unix seconds *) 
+      log: string list; (** list of log lines. This is blank unless there is an error *)
   } [@@deriving sexp]
+  (** [metadata] contains the results and parameters for a single build run of a package and version. *)
+
   type pkg =
-   { name: string;
-     mutable maintainers: string list;
-     mutable tags: string list;
-     mutable versions: (string * metadata list) list 
+   { name: string; (** opam package name *)
+     mutable maintainers: string list; (** list of maintainers associated with this package. As a heuristic, all of the maintainers listed for all versions are bundled together here for simplicity. *)
+     mutable tags: string list; (** list of tags associated with this package. As a heuristic, all of the tags listed of all versions are bundled together here for simplicity. *)
+     mutable versions: (string * metadata list) list (** list of a tuple of opam package version and the list of build results. There is a list of results since there are usually multiple different runs for every version, to test it against different compiler and CPU/OS combinations. *)
    } [@@deriving sexp]
+  (** [pkg] collects all the metadata for all versions of a given opam package. *)
+
   type pkgs = pkg list [@@deriving sexp]
+  (** [pkgs] is a full list of opam packages *)
+
   type maintainers = (string * string list) list [@@deriving sexp]
+  
   type tags = (string * string list) list [@@deriving sexp]
 end
 
