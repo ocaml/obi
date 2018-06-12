1f12e5c6c03a5a30ccfb2afaa2ba231d4c167709
Anil Madhavapeddy <anil@recoil.org>
2018-06-12 21:02:26 +0100

    Improve the opam-ci metadata error to suggest the user updates
    Signed-off-by: Anil Madhavapeddy <anil@recoil.org>

--- src/opam-ci/repos.ml
+++ src/opam-ci/repos.ml
@@ -144,6 +144,10 @@ let init ?(refresh= `Poll) () =
     with exn ->
       Printf.printf
         "We have encountered a total failure to parse the upstream metadata.\n\
+         Please run `opam update -u` to get the latest version of opam-ci \
+         that is compatible with the latest log format.\n\
+         If that does not work, you can try pinning to the development \
+         version of opam-ci via `opam pin add opam-ci --dev`.\n\
          Please report this issue with at https://github.com/ocaml/obi/issues \
          with this backtrace:\n\
          %s\n\n         \
