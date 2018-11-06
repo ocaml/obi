3d1cbf005df683d71a0154a410c0a8ed5d142d2b
Anil Madhavapeddy <anil@recoil.org>
2018-11-06 14:16:07 +0000

    improve obi-dune to generate smaller dune files
    The mechanism now uses files in the local build
    to parameterise by:
    
    - `ov` ocaml version
    - `distro` distro name
    - `rev` git revision of opam-repo
    
    There is also a @bulk alias
    
    Signed-off-by: Anil Madhavapeddy <anil@recoil.org>

--- src/obi-dune/obi_dune.ml
+++ src/obi-dune/obi_dune.ml
@@ -27,74 +27,52 @@ open R.Infix
 type build_t = {ov: Ocaml_version.t; distro: D.t}
 
 module Rules = struct
-  let base_dockerfile ~ov ~distro ~arch ~rev : string =
-    let t = D.tag_of_distro distro in
-    let f = Fmt.strf "linux-%s-%s-%s" t (OV.string_of_arch arch) rev in
-    let ov = OV.(with_patch ov None |> to_string) in
+
+  let base =
     Fmt.strf
       {|
-(rule (targets Dockerfile.%s) (action (with-stdout-to %%{targets} (
+(alias (name bulk) (deps results.tar.bz2))
+(rule (targets custom) (mode fallback) (action (write-file custom "LABEL obi.no-custom=true\n")))
+(rule (targets Dockerfile) (action (with-stdout-to %%{targets} (
     progn
-      (echo "FROM ocaml/opam2:%s\n")
+      (echo "FROM ocaml/opam2:%%{read-lines:ov}\n")
       (echo "WORKDIR /home/opam/opam-repository\n")
       (echo "RUN git pull origin master\n")
-      (echo "RUN git checkout %s\n")
+      (echo "RUN git checkout %%{read-lines:rev}\n")
       (echo "RUN opam update -uy\n")
-      (echo "RUN opam switch %s\n")
-      (echo "COPY obi-ci-install /usr/bin/obi-ci-install\n")
-)))) |}
-      f t rev ov
+      (echo "RUN opam switch %%{read-lines:ov}\n")
+      (echo "%%{read:custom}")
+      (echo "COPY obi-ci-install /usr/bin/obi-ci-install\n")))))
+(rule (targets arch) (action (with-stdout-to arch (run uname -m))))
+(rule (targets image-name)
+ (action (write-file image-name "obi-%%{read-lines:distro}_ocaml-%%{read-lines:ov}_%%{read-lines:arch}_%%{read-lines:rev}")))
+(rule (targets Dockerfile.log) (deps Dockerfile obi-ci-install)
+  (action (with-outputs-to Dockerfile.log (run docker build --pull -t %%{read:image-name} --rm --force-rm .)))) |}
 
-  let base_build ~ov ~distro ~arch ~rev : string =
-    let t = D.tag_of_distro distro in
-    let f = Fmt.strf "linux-%s-%s-%s" t (OV.string_of_arch arch) rev in
-    Fmt.strf
-      {|
-(rule (targets %s.log) (deps Dockerfile.%s obi-ci-install)
-  (action (with-stdout-to %%{targets} (run docker build -f Dockerfile.%s -t %s --rm --force-rm .)))) |}
-      f f f f
-
-  let build_one ~ov ~distro ~arch ~rev package : string =
-    let t = D.tag_of_distro distro in
-    let base = Fmt.strf "linux-%s-%s-%s" t (OV.string_of_arch arch) rev in
-    let f =
-      Fmt.strf "linux-%s-%s-%s-%s" (D.tag_of_distro distro)
-        (OV.string_of_arch arch) package rev
-    in
+  let build_one p : string =
     Fmt.strf
       {|
 (rule (targets %s.json) (deps %s.tar) (action (with-stdout-to %%{targets} (run tar -xOf %%{deps} %s.json))))
 (rule (targets %s.txt) (deps %s.tar) (action (with-stdout-to %%{targets} (run tar -xOf %%{deps} %s.txt))))
-(rule (targets %s.res) (deps %s.tar) (action (with-stdout-to %%{targets} (run tar -xOf %%{deps} %s.res))))
 
-(rule (targets %s.tar) (deps %s.log)
+(rule (targets %s.tar)
  (action (with-stdout-to %%{targets}
-    (run docker run --privileged --rm -v opam2-archive:/home/opam/.opam/download-cache %s obi-ci-install %s)))) |}
-      f f package f f package f f package f base base package
+    (run docker run --privileged --rm -v opam2-archive:/home/opam/.opam/download-cache %%{read:image-name} obi-ci-install %s)))) |}
+      p p p p p p p p
 
-  let collect ~ov ~distro ~arch ~rev packages : string =
-    let f =
-      Fmt.strf "linux-%s-%s-%s" (D.tag_of_distro distro)
-        (OV.string_of_arch arch) rev
-    in
-    let pkg ext p =
-      Fmt.strf "linux-%s-%s-%s-%s.%s" (D.tag_of_distro distro)
-        (OV.string_of_arch arch) p rev ext
-    in
+  let collect packages : string =
+    let pkg ext x = Fmt.strf "%s.%s" x ext in
     let json_deps = String.concat " " (List.map (pkg "json") packages) in
     let txt_deps = String.concat " " (List.map (pkg "txt") packages) in
-    let res_deps = String.concat " " (List.map (pkg "res") packages) in
     Fmt.strf
       {|
-(rule (targets %s.tar.bz2) (deps %s %s %s) (action (run tar -jcf %%{targets} %%{deps})))
-|}
-      f json_deps txt_deps res_deps
+(rule (targets results.tar.bz2) (deps %s %s arch ov rev distro) (action (run tar -jcf %%{targets} %%{deps})))
+|} json_deps txt_deps
 
-  let gen ~ov ~distro ~arch ~rev packages =
-    [ base_dockerfile ~ov ~distro ~arch ~rev
-    ; base_build ~ov ~distro ~arch ~rev
-    ; collect ~ov ~distro ~arch ~rev packages ]
-    @ List.map (build_one ~ov ~distro ~arch ~rev) packages
+  let gen packages =
+    [ base
+    ; collect packages ]
+    @ List.map build_one packages
     |> String.concat "\n"
 end
 
@@ -109,53 +87,24 @@ let packages ~rev () =
             rev)
     |> to_lines)
 
-let gen_bulk_rules {ov; distro} () =
+let gen_bulk_rules () =
   let rev = "master" in
-  let arch = `X86_64 in
   prerr_endline "Generating package list..." ;
   packages ~rev ()
   >>= fun packages ->
   prerr_endline (Fmt.strf "... %d." (List.length packages)) ;
-  Rules.gen ~ov ~distro ~arch ~rev packages
+  Rules.gen packages
   |> fun dune -> prerr_endline dune ; Ok ()
 
 open Cmdliner
 
 let setup_logs = C.setup_logs ()
 
-let fpath = Arg.conv ~docv:"PATH" (Fpath.of_string, Fpath.pp)
-
-let buildv ov distro =
-  Ocaml_version.of_string_exn ov
-  |> fun ov ->
-  let distro =
-    match D.distro_of_tag distro with
-    | None -> failwith "unknown distro"
-    | Some distro -> distro
-  in
-  {ov; distro}
-
-let build_t =
-  let ocaml_version =
-    let doc = "ocaml version to build" in
-    let env = Arg.env_var "OCAML_VERSION" ~doc in
-    let open Arg in
-    value & opt string "4.07.1"
-    & info ["ocaml-version"] ~docv:"OCAML_VERSION" ~env ~doc
-  in
-  let distro =
-    let doc = "distro to build" in
-    let env = Arg.env_var "DISTRO" ~doc in
-    let open Arg in
-    value & opt string "debian-9" & info ["distro"] ~env ~docv:"DISTRO" ~doc
-  in
-  Term.(const buildv $ ocaml_version $ distro)
-
 let opam_cmd =
   let doc = "generate bulk build dune rules for a particular opam repo rev" in
   let exits = Term.default_exits in
   let man = [`S Manpage.s_description; `P "TODO"] in
-  ( Term.(term_result (const gen_bulk_rules $ build_t $ setup_logs))
+  ( Term.(term_result (const gen_bulk_rules $ setup_logs))
   , Term.info "rules" ~doc ~sdocs:Manpage.s_common_options ~exits ~man )
 
 let default_cmd =
